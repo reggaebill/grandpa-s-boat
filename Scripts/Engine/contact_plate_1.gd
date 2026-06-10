bf498f8a7a7d50b5993ea579bed341ab488167b5
Hawkjedi <thomas@ddavidn.net>
2026-06-09 17:19:10 -0700

    Engine updates
    Engine stuff

---
+++ Scripts/Engine/contact_plate_1.gd
@@ -0,0 +1,48 @@
+extends Area2D
+
+## Example of the lit tubes script from Whale's level:
+
+var wire_entered = false
+var active_wire = null
+
+@export var yellowWire: Sprite2D
+@export var greenWire: Sprite2D
+@export var redWire: Sprite2D
+
+#@onready var litTube = $Sprite2D
+#
+func _ready() -> void:
+	pass
+
+#func _process(delta: float) -> void:
+	##verify which tube entered and handle accordingly
+	#if tube_entered == true and active_tube == tube_1 and tube_1.position.distance_to(position) < 35:	
+		#if tube_1.dragging == false:
+			#tube_1.visible = false
+			#litTube.visible = true
+	#elif tube_entered == true and active_tube == tube_2 and tube_2.position.distance_to(position) < 35:
+		#if litTube.visible == true:
+			#pass
+		#elif tube_2.dragging == true:
+			#pass
+		#elif tube_2.dragging == false:
+			#tube_2.position = position
+	#elif tube_entered == true and active_tube == tube_3 and tube_3.position.distance_to(position) < 35:
+		#if litTube.visible == true:
+			#pass
+		#elif tube_3.dragging == true:
+			#pass
+		#elif tube_3.dragging == false:
+			#tube_3.position = position
+#
+#
+#func _on_area_entered(area: Area2D) -> void:
+	#if area.name == "Area2DTube1":
+		#active_tube = tube_1
+		#tube_entered = true
+	#elif area.name == "Area2DTube2":
+		#active_tube = tube_2
+		#tube_entered = true
+	#elif area.name == "Area2DTube3":
+		#active_tube = tube_3
+		#tube_entered = true
