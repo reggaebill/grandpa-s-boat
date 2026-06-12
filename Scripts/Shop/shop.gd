d6539531a73ea292b679eb62510331da55ad8f65
Hawkjedi <thomas@ddavidn.net>
2026-06-12 12:33:21 -0700

    Updating Shop
    Added money counter and sell button to shop script/scene.

--- Scripts/Shop/shop.gd
+++ Scripts/Shop/shop.gd
@@ -1,6 +1,15 @@
 extends Node2D
+# Shop level to handle buying and selling. I'm planning on making this just a bunch of state.gd flag changes.
 
+@onready var moneyAmount: Label = $MoneyCounter/MoneyMargin/HBoxContainer/Label
+
+func _ready() -> void:
+	# When scene loads, set the money display to show whatever is stored in state.gd.
+	moneyAmount.text = str("$",State.money)
 
 func _on_back_button_pressed() -> void:
 	print("back")
 	SceneTransition.change_scene("res://Scenes/Boat Overview.tscn")
+
+func _on_sell_button_pressed() -> void:
+	pass # Replace with function body.
--- Scripts/state.gd
+++ Scripts/state.gd
@@ -6,8 +6,13 @@ extends Node
 
 # Engine states.
 var enginePartsPurchased:= false
-var yellowGoal = false
-var greenGoal = false
-var redGoal = false
+var yellowGoal:= false
+var greenGoal:= false
+var redGoal:= false
 var engineFixed:= false
 var cardCollected:= false
+
+# Shop states.
+var money: int = 0
+
+var cardSold:= false
