96a5da309b806244e96469f02495c1fb7a1c045f
reggaebill <aarongerhart7@gmail.com>
2026-06-13 00:05:07 -0400

    test magnet fishing

---
+++ Scenes/Test/control.gd
@@ -0,0 +1,24 @@
+extends Control
+
+pass
+
+# var dragging = false
+# var of = Vector2(0, 0)
+# # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+# func _process(_delta):
+# 	if dragging:
+# 		position = position.lerp(get_global_mouse_position() - of, 0.2)
+# 		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+# 	else:
+# 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+# func _on_button_button_down():
+# 	dragging = true
+# 	of = get_global_mouse_position() - global_position
+
+
+
+# func _on_button_button_up():
+# 	dragging = false
+
---
+++ Scenes/Test/rigid_body_2d.gd
@@ -0,0 +1,26 @@
+extends RigidBody2D
+
+
+var dragging = false
+var of = Vector2(0, 0)
+var last_linear_velocity = Vector2.ZERO
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _process(_delta):
+	if dragging:
+		position = position.lerp(get_global_mouse_position() - of, 0.2)
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func _on_button_button_down():
+	dragging = true
+	of = get_global_mouse_position() - global_position
+
+
+
+func _on_button_button_up():
+	dragging = false
+
+func _integrate_forces(state):
+	last_linear_velocity = state.linear_velocity
